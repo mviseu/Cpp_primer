60c1eef8114aebb4c7411750553b888bf33e547f
mviseu <mariaviseu@gmail.com>
2017-05-09 22:07:33 +0100

    main handling command line options

---
+++ Chapter6/6_26.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main(int argc, char **argv) {
+  string sTotal;
+  for (auto row = 0; row < argc; ++row) {
+    string sRow(argv[row]);
+    sTotal += sRow + " ";
+  }
+
+  cout << sTotal << endl;
+
+  return 0;
+}
