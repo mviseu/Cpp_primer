ebc43485d4aea9ab0621128a9bc01f934a6eaf32
mviseu <mariaviseu@gmail.com>
2017-05-13 22:47:21 +0100

    Default parameters (2)

---
+++ Chapter6/6_46.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+string makePlural(size_t ctr, const string &word, const string &ending = "s") {
+  return ctr > 1 ? word + ending : word;
+}
+
+int main() { 
+
+	cout << makePlural(2, "Success", "es") << endl;
+	cout << makePlural(2, "Failure") << endl;
+	cout << makePlural(2, "Failure") << endl;
+
+	return 0; 
+
+}
