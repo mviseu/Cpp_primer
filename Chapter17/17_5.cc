0b6de2ffd805574fa753fefa1ec48535d8c953a3
Maria Nunes de Almeida Viseu <mariaviseu@gmail.com>
2017-12-19 19:22:29 +0000

    Add reportResults function:
    - for tuple type
    - for pair of index, pair type
    - for my own data struct

--- Chapter17/17_5.cc
+++ Chapter17/17_5.cc
@@ -5,6 +5,7 @@
 #include <iterator>
 #include <tuple>
 #include <vector>
+#include <numeric>
 
 // using a pair that holds an index and a pair of iterators instead
 
@@ -22,12 +23,32 @@ std::vector<matches> findBook(const std::vector<std::vector<Sales_data>> &trans,
 	return res;
 }
 
+void reportResults(std::istream &is, std::ostream &os, const std::vector<std::vector<Sales_data>> &trans) {
+	std::string s;
+	while(is >> s) {
+		auto find_book = findBook(trans, s);
+		if(find_book.empty()) {
+			os << "There are no transactions for book: " << s << std::endl;
+			continue;
+		}
+		for(const auto &elem : find_book) {
+			os << "For book " << s << ", the store with index " << elem.first 
+			   << " has following transaction sum: \n" << std::accumulate(elem.second.first, elem.second.second, Sales_data(s))
+			   << std::endl; 
+		}
+	}
+}
+
 
 int main() {
 	Sales_data S1("XX", 3, 2);
 	Sales_data S2("XX", 2, 2);
-	std::unordered_multiset<Sales_data> setSales({S1, S2, S1 + S2});
+	Sales_data S3("XY", 3, 2);
+	Sales_data S4("XY", 2, 2);
+	std::vector<Sales_data> store1 = {S1, S2, S3};
+	std::vector<Sales_data> store2 = {S4};
+	std::vector<std::vector<Sales_data>> all = {store1, store2};
 
-	std::copy(setSales.begin(), setSales.end(), std::ostream_iterator<Sales_data>(std::cout));
+	reportResults(std::cin, std::cout, all);
 	return 0;
 }
--- Chapter17/17_6.cc
+++ Chapter17/17_6.cc
@@ -5,6 +5,7 @@
 #include <iterator>
 #include <tuple>
 #include <vector>
+#include <numeric>
 
 //uses my own struct
 
@@ -30,12 +31,31 @@ std::vector<Match> findBook(const std::vector<std::vector<Sales_data>> &trans, c
 	return res;
 }
 
+void reportResults(std::istream &is, std::ostream &os, const std::vector<std::vector<Sales_data>> &trans) {
+	std::string s;
+	while(is >> s) {
+		auto find_book = findBook(trans, s);
+		if(find_book.empty()) {
+			os << "There are no transactions for book: " << s << std::endl;
+			continue;
+		}
+		for(const auto &elem : find_book) {
+			os << "For book " << s << ", the store with index " << elem.index 
+			   << " has following transaction sum: \n" << std::accumulate(elem.begin, elem.end, Sales_data(s))
+			   << std::endl; 
+		}
+	}
+}
 
 int main() {
 	Sales_data S1("XX", 3, 2);
 	Sales_data S2("XX", 2, 2);
-	std::unordered_multiset<Sales_data> setSales({S1, S2, S1 + S2});
+	Sales_data S3("XY", 3, 2);
+	Sales_data S4("XY", 2, 2);
+	std::vector<Sales_data> store1 = {S1, S2, S3};
+	std::vector<Sales_data> store2 = {S4};
+	std::vector<std::vector<Sales_data>> all = {store1, store2};
 
-	std::copy(setSales.begin(), setSales.end(), std::ostream_iterator<Sales_data>(std::cout));
+	reportResults(std::cin, std::cout, all);
 	return 0;
 }
